a3ed56c06df139257e38a985869593d808920fa7
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-10 00:33:27 +0200

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+// Define the divide function expression
+function divide(x, y) {
+    return x / y;
+  }
+  
+  // Define the square arrow function
+  const square = (x) => x * x;
+  
+  // Define the add arrow function
+  const add = (a, b) => a + b;
+  
+  // Export the functions
+  module.exports = {
+    divide,
+    square,
+    add,
+  };
+  
